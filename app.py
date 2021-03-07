56e1303f07b052add40e3120e9a8288f2ba62b9e
Felix Luciano Salomon <felixluciano.a@gmail.com>
2021-03-07 18:29:33 +0000

    Added imports for FlaskSqlAlchemy

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, send_from_directory, json, session,request
 from flask_socketio import SocketIO
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv,find_dotenv
 
 app = Flask(__name__, static_folder='./build/static')
 
