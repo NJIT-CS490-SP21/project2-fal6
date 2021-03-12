02cf8e104aeddc1fb7d4d7d411d1b742009fe547
Felix Luciano <felixluciano.a@gmail.com>
2021-03-11 22:11:59 -0500

    Linted app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
-from flask import Flask, send_from_directory, json, session,request
-from flask_socketio import SocketIO,emit
+from flask import Flask, send_from_directory, json, request
+from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv,find_dotenv
+from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
@@ -14,135 +14,150 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 import models
 
-
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
-socketio = SocketIO(
-    app,
-    cors_allowed_origins="*",
-    json=json,
-    manage_session=False
-)
+socketio = SocketIO(app,
+                    cors_allowed_origins="*",
+                    json=json,
+                    manage_session=False)
 
-board = [[None,None,None] for i in range(3)] #Tic tac toe board
-turn = False #Current player turn (0,X) (1, O)
-players = [] # List of two current players
-spectators = {} # List of spectators
-valid_ids = [] # List of the two player Ids
-users = {}
-game = False # Keeps track of if the game has started
-win = False # Keeps track of if the user has won
+BOARD = [[None, None, None] for i in range(3)]  #Tic tac toe board
+TURN = False  #Current player turn (0,X) (1, O)
+PLAYERS = []  # List of two current players
+SPECTATORS = {}  # List of spectators
+VALID_IDS = []  # List of the two player Ids
+USERS = {}
+GAME = False  # Keeps track of if the game has started
+WIN = False  # Keeps track of if the user has won
+
 
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
 def index(filename):
+    '''index'''
     return send_from_directory('./build', filename)
+
 
 # When a client connects from this Socket connection, this function is run
 @socketio.on('connect')
 def on_connect():
+    '''When a user connects'''
     print('User connected!')
-    
+
 
 # When a client disconnects from this Socket connection, this function is run
 @socketio.on('disconnect')
 def on_disconnect():
     '''Clean up when a user disconnects'''
-    global game
-    global valid_ids
-    global board
-    global win
-    global turn
-    if (request.sid in valid_ids): #If the user is a player, remove players
-        players.clear() 
-        spectators.clear()
-        users.clear()
-        game = False
-        win = False
-        turn = False
-        valid_ids.pop(valid_ids.index(request.sid))
-        board = [[None,None,None] for i in range(3)]
+    global GAME
+    global VALID_IDS
+    global BOARD
+    global WIN
+    global TURN
+    if request.sid in VALID_IDS:  #If the user is a player, remove players
+        PLAYERS.clear()
+        SPECTATORS.clear()
+        USERS.clear()
+        GAME = False
+        WIN = False
+        TURN = False
+        VALID_IDS.pop(VALID_IDS.index(request.sid))
+        BOARD = [[None, None, None] for i in range(3)]
     print('User disconnected!')
-    print(valid_ids)
+    print(VALID_IDS)
+
 
 @socketio.on('reset')
 def on_reset():
     '''Resets turn and board'''
-    global turn 
-    global win
-    for i in range(len(board)):
-        board[i] = [None,None,None]
-    turn = 0
-    win = False
+    global TURN
+    global WIN
+    for count,_ in enumerate(BOARD):
+        BOARD[count] = [None, None, None]
+    TURN = 0
+    WIN = False
     data = {
-        'board':board,'turn':turn,
-        'spectators':spectators,'win':win,
+        'board': BOARD,
+        'turn': TURN,
+        'spectators': SPECTATORS,
+        'win': WIN,
     }
-    socketio.emit("init",data,broadcast=True,include_self=True)
+    socketio.emit("init", data, broadcast=True, include_self=True)
+
 
 @socketio.on('login')
 def on_login(data):
     '''Log user or spectator in'''
-    global game
-    if(db.session.query(models.Player).filter_by(username=data["name"]).first() is not None):
+    global GAME
+    if (db.session.query(
+            models.Player).filter_by(username=data["name"]).first()
+            is not None):
         print("The user exists")
     else:
-        new_user=models.Player(username=data["name"],points=100)
+        new_user = models.Player(username=data["name"], points=100)
         db.session.add(new_user)
         db.session.commit()
         print(models.Player.query.all())
-    if len(players) <2:
-        players.append({request.sid:data["name"]})
-        valid_ids.append(request.sid)
-        print(players)
+    if len(PLAYERS) < 2:
+        PLAYERS.append({request.sid: data["name"]})
+        VALID_IDS.append(request.sid)
+        print(PLAYERS)
     send_data = {
-        'board':board,'turn':turn,
-        'spectators':spectators,'win':win,
+        'board': BOARD,
+        'turn': TURN,
+        'spectators': SPECTATORS,
+        'win': WIN,
     }
-    emit("init",send_data) #initializes board on connect
-    users[request.sid] = data["name"]
-    if game:
-        spectators[request.sid]=data["name"]
-        socketio.emit("spectator",{
-            "spectators":list(spectators.values())})
-    if len(players)==2:
-        socketio.emit("game",{"players":players,"ids":valid_ids})
-        game = True
-    
+    emit("init", send_data)  #initializes board on connect
+    USERS[request.sid] = data["name"]
+    if GAME:
+        SPECTATORS[request.sid] = data["name"]
+        socketio.emit("spectator", {"spectators": list(SPECTATORS.values())})
+    if len(PLAYERS) == 2:
+        socketio.emit("game", {"players": PLAYERS, "ids": VALID_IDS})
+        GAME = True
 
 
 @socketio.on("click")
 def on_click(data):
     '''Updates board when a box is clicked'''
     print(str(data))
-    global turn
-    turn = not turn
-    indx=data['message']
-    board[indx[0]][indx[1]]=data['shape']
-    socketio.emit("click",data,broadcast=True,include_self=False)
+    global TURN
+    TURN = not TURN
+    indx = data['message']
+    BOARD[indx[0]][indx[1]] = data['shape']
+    socketio.emit("click", data, broadcast=True, include_self=False)
+
 
 @socketio.on("win")
 def on_win(data):
-    user=db.session.query(models.Player).filter_by(
-        username=players[data["winner"]][valid_ids[data["winner"]]]).first()
-    user.points+=1
-    user2=db.session.query(models.Player).filter_by(
-        username=players[not data["winner"]][valid_ids[not data["winner"]]]).first()
-    user2.points-=1
+    '''Update database when user wins'''
+    user = db.session.query(models.Player).filter_by(
+        username=PLAYERS[data["winner"]][VALID_IDS[data["winner"]]]).first()
+    user.points += 1
+    user2 = db.session.query(
+        models.Player).filter_by(username=PLAYERS[not data["winner"]][
+            VALID_IDS[not data["winner"]]]).first()
+    user2.points -= 1
     db.session.commit()
+
 
 @socketio.on("leaderboard")
 def on_leaderboard():
+    '''Returns a list of users and scores sorted from first to last'''
     print(request.sid)
     leaderboard = models.Player.query.all()
     leaderboard = list(
-        map(lambda person:[person.username,person.points],
-        leaderboard))
-    leaderboard.sort(key=lambda leaderboard:leaderboard[1],reverse=True)
-    
-    emit("leaderboard",{'leaderboard':leaderboard,'name':users[request.sid]})
+        map(lambda person: [person.username, person.points], leaderboard))
+    leaderboard.sort(key=lambda leaderboard: leaderboard[1], reverse=True)
+
+    emit("leaderboard", {
+        'leaderboard': leaderboard,
+        'name': USERS[request.sid]
+    })
+
 
 # Note that we don't call app.run anymore. We call socketio.run with app arg
-if __name__=="__main__":
+if __name__ == "__main__":
     socketio.run(
         app,
         host=os.getenv('IP', '0.0.0.0'),
--- models.py
+++ models.py
@@ -1,10 +1,17 @@
+'''
+Stores Model for our database
+'''
 from app import db
 
+
 class Player(db.Model):
+    '''Stores username and points for a player'''
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     points = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return '<Player %r>' % self.username
+
+
 db.create_all()
